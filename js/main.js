80169c7d3d3ff7e36ad32e6e7ea08dffb65fdd10
misom.bae <meesomee@naver.com>
2023-01-25 17:49:08 +0900

    carousel 수정

--- js/main.js
+++ js/main.js
@@ -98,16 +98,36 @@
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
+        // autoplay: true,
+        // smartSpeed: 1500,
+        // items: 1,
+        // dots: false,
+        // loop: true,
+        // nav : true,
+        // navText : [
+        //     '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        //     '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        // ],
         autoplay: true,
         smartSpeed: 1500,
-        items: 1,
-        dots: false,
+        margin: 30,
+        dots: true,
         loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
+        center: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
     });
     
 })(jQuery);
